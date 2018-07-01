286ce18a4a694250f03e68dbf6fdcb6a58d1a95c
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-06-30 21:35:07 -0600

    new queries

--- schema.sql
+++ schema.sql
@@ -1,1 +1,19 @@
 SELECT userId, AVG(duration) FROM sessions GROUP BY userId HAVING COUNT(userId)>1;
+
+
+SELECT MAX(TotalSalesPrice), TranID FROM tsalesorders;
+
+
+SELECT TotalSalesPrice, TranID FROM tsalesorders WHERE TotalSalesPrice > 50;
+
+SELECT tsalesorders.SOKey FROM tsalesorders
+INNER JOIN tcustomers
+ON tcustomers.CustomerKey = tsalesorders.CustomerKey
+WHERE tcustomers.FirstName = 'Floyd' AND LastName = 'Mayweather';
+
+SELECT titems.ItemID FROM titems
+INNER JOIN tsalesorderlines
+ ON titems.ItemID = tsalesorderlines.ItemKey
+INNER JOIN tsalesorders
+ ON tsalesorderlines.SOKey = tsalesorders.SOKey
+WHERE tsalesorders.TranID = 000000002;
